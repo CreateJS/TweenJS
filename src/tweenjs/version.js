12f405244898b65cefa6a9a5dc7eb803ea82bbd2
Lanny McNie <lanny@gskinner.com>
2013-08-01 11:04:46 -0600

    Updated versions file to address namespace  & strict
    Signed-off-by: Lanny McNie <lanny@gskinner.com>

--- src/tweenjs/version.js
+++ src/tweenjs/version.js
@@ -1,27 +1,32 @@
+/**
+ * @module TweenJS
+ */
+this.createjs = this.createjs || {};
+
 (function() {
+	"use strict";
 
-/**
- * Static class holding library specific information such as the version and buildDate of
- * the library.
- * @class TweenJS
- **/
-var o = this.createjs = this.createjs||{};
-o = (o.TweenJS = o.TweenJS||{});
- 
-/**
- * The version string for this release.
- * @property version
- * @type String
- * @static
- **/
-o.version = /*version*/"NEXT"; // injected by build process
+	/**
+	 * Static class holding library specific information such as the version and buildDate of
+	 * the library.
+	 * @class TweenJS
+	 **/
+	var s = createjs.TweenJS = createjs.TweenJS || {};
 
-/**
- * The build date for this release in UTC format.
- * @property buildDate
- * @type String
- * @static
- **/
-o.buildDate = /*date*/"Wed, 24 Jul 2013 01:17:39 GMT"; // injected by build process
+	/**
+	 * The version string for this release.
+	 * @property version
+	 * @type String
+	 * @static
+	 **/
+	s.version = /*version*/"NEXT"; // injected by build process
+
+	/**
+	 * The build date for this release in UTC format.
+	 * @property buildDate
+	 * @type String
+	 * @static
+	 **/
+	s.buildDate = /*date*/"Mon, 22 Jul 2013 21:05:51 GMT"; // injected by build process
 
 })();
