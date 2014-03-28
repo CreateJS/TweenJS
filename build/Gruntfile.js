33e458ce21047e770ba66854898c93699385944a
Lanny McNie <lanny@gskinner.com>
2014-03-28 13:57:36 -0600

    Added bower support, including a grunt task for automatic updates to bower.json

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -208,7 +208,7 @@ module.exports = function (grunt) {
 	 *
 	 */
 	grunt.registerTask('build', [
-		"setVersion", "coreBuild", "copy:docsSite"
+		"setVersion", "coreBuild", "updatebower", "copy:docsSite"
 	]);
 
 	/**
