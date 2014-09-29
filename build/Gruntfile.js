a103d6513dfaff903cd0eecf450920b0ebd1e005
Wes Gorgichuk <wes@gskinner.com>
2014-09-29 14:51:29 -0600

    During a build only copy the current working version of the javascript to ../lib/

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -89,7 +89,7 @@ module.exports = function (grunt) {
 					},
 					src: {
 						files: [
-							{expand: true, cwd:'./output/', src: '*.js', dest: '../lib/'}
+							{expand: true, cwd:'./output/', src: '*<%=version %>*.js', dest: '../lib/'}
 						]
 					}
 				},
