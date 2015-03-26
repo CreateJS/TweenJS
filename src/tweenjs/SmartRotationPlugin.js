c074c6351e9e7c9f84046704f2894bbe1bf2c100
Grant Skinner <info@gskinner.com>
2015-03-25 22:18:59 -0600

    Added SmartRotationPlugin
    Signed-off-by: Grant Skinner <info@gskinner.com>

---
+++ src/tweenjs/SmartRotationPlugin.js
@@ -0,0 +1,70 @@
+/*
+* SmartRotationPlugin
+* Visit http://createjs.com/ for documentation, updates and examples.
+*
+* Copyright (c) 2010 gskinner.com, inc.
+*
+* Permission is hereby granted, free of charge, to any person
+* obtaining a copy of this software and associated documentation
+* files (the "Software"), to deal in the Software without
+* restriction, including without limitation the rights to use,
+* copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following
+* conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+* OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*
+	IMPORTANT: This class is not documented yet.
+
+	The SmartRotationPlugin will ensure rotation tweens will always take the shortest path.
+	Installing this plugin will currently add this behaviour to all tweens.
+ */
+this.createjs = this.createjs||{};
+
+(function() {
+	"use strict";
+
+	function SmartRotationPlugin() {
+		throw("SmartRotation plugin cannot be instantiated.")
+	}
+	
+	SmartRotationPlugin.props = {rotation:1, rotationX:1, rotationY:1, rotationZ:1};
+
+	SmartRotationPlugin.install = function() {
+		createjs.Tween.installPlugin(SmartRotationPlugin);
+	};
+
+	SmartRotationPlugin.init = function(tween, prop, value) {
+		return value;
+	};
+	
+	SmartRotationPlugin.step = function(tween, step, prop, value, injectProps) {
+		if (!SmartRotationPlugin.props[prop]) { return; }
+		var start = step.prev.props[prop];
+		var delta = (value-start)%360;
+		if (delta > 180) { delta -= 360; }
+		else if (delta < -180) { delta += 360; }
+		step.props[prop] = start+delta;
+	};
+
+	SmartRotationPlugin.tween = function(tween, step, prop, value, ratio, end) {
+		// do nothing.
+		return value;
+	};
+
+	createjs.SmartRotationPlugin = SmartRotationPlugin;
+
+}());
